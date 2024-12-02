94f901c2b3008e61ab80c9de97f0386a0239006a
AISTATS2025 <x849038705@gmail.com>
2024-12-02 22:55:37 +0800

    Add files via upload

--- main.m
+++ main.m
@@ -155,7 +155,7 @@ for mtcl = 1:100
                 %[Theta{nAgent, gr, iter}, optDist{nAgent, gr, iter}, obj{nAgent, gr, iter}, check] = Network_GGM_par(xs{nAgent, iter}, ns(iter), m, d, lambda, tau, alpha, max_iter, 1e-7, 1e-7, invsigma, Wxs{gr});
                 times(nAgent, gr + 1, iter) = toc;
                 toc
-                while check && norm(kron(ones(1,m), eye(d)) * Theta{nAgent, gr, iter} / m - invsigma, 'fro') ^ 2 / norm(invsigma, 'fro') ^ 2 - minNMSE >= 1e-6
+                while check && norm(kron(ones(1,m), eye(d)) * Theta{nAgent, gr, iter} / m - invsigma, 'fro') ^ 2 / norm(invsigma, 'fro') ^ 2 - minNMSE >= 1e-8
                     if nAgent == 1
                         if gr == 1
                             tau = 1.25 * tau;
@@ -195,7 +195,7 @@ for mtcl = 1:100
                 %[Theta{nAgent, gr, iter}, optDist{nAgent, gr, iter}, obj{nAgent, gr, iter}, check] = Network_GGM_par(xs{nAgent, iter}, ns(iter), m, d, lambda, tau, alpha, max_iter, 1e-7, 1e-7, invsigma, Wxs{gr});
                 times_L(nAgent, gr, iter) = toc;
                 toc
-                while check && norm(kron(ones(1,m), eye(d)) * Theta_L{nAgent, gr, iter} / m - invsigma, 'fro') ^ 2 / norm(invsigma, 'fro') ^ 2 - minNMSE >= 1e-6
+                while check && norm(kron(ones(1,m), eye(d)) * Theta_L{nAgent, gr, iter} / m - invsigma, 'fro') ^ 2 / norm(invsigma, 'fro') ^ 2 - minNMSE >= 1e-8
                     if nAgent == 1
                         if gr == 1
                             tau = 1.25 * tau;
@@ -235,7 +235,7 @@ for mtcl = 1:100
                 %[Theta{nAgent, gr, iter}, optDist{nAgent, gr, iter}, obj{nAgent, gr, iter}, check] = Network_GGM_par(xs{nAgent, iter}, ns(iter), m, d, lambda, tau, alpha, max_iter, 1e-7, 1e-7, invsigma, Wxs{gr});
                 times_M(nAgent, gr, iter) = toc;
                 toc
-                while check && norm(kron(ones(1,m), eye(d)) * Theta_M{nAgent, gr, iter} / m - invsigma, 'fro') ^ 2 / norm(invsigma, 'fro') ^ 2 - minNMSE >= 1e-6
+                while check && norm(kron(ones(1,m), eye(d)) * Theta_M{nAgent, gr, iter} / m - invsigma, 'fro') ^ 2 / norm(invsigma, 'fro') ^ 2 - minNMSE >= 1e-8
                     if nAgent == 1
                         if gr == 1
                             tau = 1.25 * tau;
@@ -273,7 +273,7 @@ for mtcl = 1:100
         
     end
     name = "NetGGM_mtcl=" + int2str(mtcl)
-    save(name + ".mat", 'Theta_opt', 'Theta', 'Theta_L', 'Theta_M', 'times', 'times_L', 'times_M')
+    save(name + ".mat", 'Theta_opt', 'Theta_bl', 'Theta', 'Theta_L', 'Theta_M', 'times', 'times_L', 'times_M')
 end
 
 function [random_integers] = rand_sum(total_sum, num_elements)
